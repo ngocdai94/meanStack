82ea46588807314b76b9aa3db18bb54ca88ac9a5
Dai Nguyen <ngocdai94@gmail.com>
2020-11-25 20:40:42 -0800

    response index.html page

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
-const express = require('express');
+'use strict'
+
+// Import necessary packages and js file for the application
 const MongoClient = require('mongodb').MongoClient;
+const express = require('express');
 const app = express();
 
+// Set location for static files
+app.use(express.static(__dirname + '/public/it115-a5-g2-app')); 
+
+
 app.use('/', (req, res) => {
-    MongoClient.connect("mongodb://localhost:27017/test", function(err, db){
-        db.collection('Example', function(err, collection){
-            collection.insert({ pageHits: 'pageHits' });
-            db.collection('Example').count(function(err, count){
-                if(err) throw err;
-                res.status(200).send('Page Hits: ' + Math.floor(count/2));
-            });
-        });
-    });
+    // MongoClient.connect("mongodb://localhost:27017/test", function(err, db){
+    //     db.collection('Example', function(err, collection){
+    //         collection.insert({ pageHits: 'pageHits' });
+    //         db.collection('Example').count(function(err, count){
+    //             if(err) throw err;
+    //             res.status(200).send('Page Hits: ' + Math.floor(count/2));
+    //         });
+    //     });
+    // });
+
+    res.type('text/html');
+    res.render('index');
 });
 
 app.listen(80);
