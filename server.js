36d3f5e6d3c19a9678575bf549f64b3311b80f0f
Dai Nguyen <ngocdai94@gmail.com>
2020-11-25 21:35:24 -0800

    response to index,html request

--- server.js
+++ server.js
@@ -1,27 +1,18 @@
-'use strict'
+'use strict';
 
 // Import necessary packages and js file for the application
-const MongoClient = require('mongodb').MongoClient;
 const express = require('express');
 const app = express();
+const path = require('path');
+const MongoClient = require('mongodb').MongoClient;
 
 // Set location for static files
 app.use(express.static(__dirname + '/public/it115-a5-g2-app')); 
 
 
 app.use('/', (req, res) => {
-    // MongoClient.connect("mongodb://localhost:27017/test", function(err, db){
-    //     db.collection('Example', function(err, collection){
-    //         collection.insert({ pageHits: 'pageHits' });
-    //         db.collection('Example').count(function(err, count){
-    //             if(err) throw err;
-    //             res.status(200).send('Page Hits: ' + Math.floor(count/2));
-    //         });
-    //     });
-    // });
-
     res.type('text/html');
-    res.render('index');
+    res.sendFile(path.join(__dirname + '/public/it115-a5-g2-app/index.html'));
 });
 
 app.listen(80);
