0ddfd6378aa20c20cdf52e3bfc9e9b467385e0c5
Dai Nguyen <ngocdai94@gmail.com>
2020-11-26 00:36:50 +0000

    Mean Stack First Init

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-'use strict';
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const app = express();
 
-/**
- * Module dependencies.
- */
-var app = require('./config/lib/app');
-var server = app.start();
+app.use('/', (req, res) => {
+    MongoClient.connect("mongodb://localhost:27017/test", function(err, db){
+        db.collection('Example', function(err, collection){
+            collection.insert({ pageHits: 'pageHits' });
+            db.collection('Example').count(function(err, count){
+                if(err) throw err;
+                res.status(200).send('Page Hits: ' + Math.floor(count/2));
+            });
+        });
+    });
+});
+
+app.listen(80);
+console.log('Server running at http://localhost:80/');
+
+module.exports = app;
